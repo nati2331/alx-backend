ebc5318fc7ccd85d09ef26498fe24acf6cdd9306
nati2331 <natnael.mk2@gmail.com>
2024-11-08 06:29:54 +0000

    reviewed question 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,42 +1,37 @@
 #!/usr/bin/env python3
+"""Flask app
 """
-Parametrize templates with language support
-"""
-
-from flask import Flask, render_template, request
 from flask_babel import Babel
-
-app = Flask(__name__)
-babel = Babel(app)
+from flask import Flask, render_template, request
 
 
 class Config:
+    """Flask Babel configuration.
     """
-    Config class for language and locale settings
-    """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
+    """Returns web page.
     """
-    Determine the best match with supported languages
-    """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-@app.route('/', methods=['GET'])
-def index():
-    """
-    Render the home page with translations
+@app.route('/')
+def get_index() -> str:
+    """Index page.
     """
     return render_template('3-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
